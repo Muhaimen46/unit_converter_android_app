358cc9ca289e4f12d7404df0b9086b16e8897784
Muhaimen46 <khan1707046@stud.kuet.ac.bd>
2021-11-17 09:26:37 +0600

    Arno file added

---
+++ app/src/main/java/anubhav/calculatorapp/ArnoFile.java
@@ -0,0 +1,36 @@
+package anubhav.calculatorapp;
+
+public class ArnoFile {
+
+    int x,y,z;
+
+    public ArnoFile(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+}
