1c928da28a58e19383967e2804f6fbcb1f731687
Ravendra <kumarravendra058@gmail.com>
2021-11-17 07:21:55 +0530

    commit

---
+++ app/src/main/java/anubhav/calculatorapp/Splash_screen.java
@@ -0,0 +1,23 @@
+package anubhav.calculatorapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(Splash_screen.this,UnitCoverter.class);
+                startActivity(i);
+            }
+        },3000);
+    }
+}
